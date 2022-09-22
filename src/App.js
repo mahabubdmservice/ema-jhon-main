1efcc2800fed7bb0b1186bd952413f86e957fe14
mahabubdmservice <mahabuburrahman.jsr@gmail.com>
2022-09-22 16:54:23 +0600

    Solved fakeData pblm

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import fakeData from'./fakeData/products.json';
 
 function App() {
+  console.log(fakeData);
   return (
     <div>
       <Header></Header>
